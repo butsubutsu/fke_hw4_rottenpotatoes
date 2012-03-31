b9ef7d913b5a6722d71662135d944e17ca3daa4b
Andrea Scarcella <scarcelan@gmail.com>
2012-03-31 03:11:01 -0700

    added db migration added cucumber feature file

---
+++ db/migrate/20120331_director_col.rb
@@ -0,0 +1,9 @@
+class DirectorCol < ActiveRecord::Migration
+  def up
+    add_column :movies, :director, :string, :default => ''
+  end
+
+  def down
+    remove_column :movies, :director
+  end
+end
