d338bcca09b8b0fc0e1e740561a01b142c4eb57b
Andrea Scarcella <scarcelan@gmail.com>
2012-04-01 07:11:46 -0700

    added path for edit pages

--- features/step_definitions/director_steps.rb
+++ features/step_definitions/director_steps.rb
@@ -1,4 +1,7 @@
 Given /^the following movies exist:$/ do |table|
   # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
+  table.hashes.each do |movie|
+    Movie.create!(movie)
+  end
 end
