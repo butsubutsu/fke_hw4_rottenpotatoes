eb23710c0339db8047078c2a1f30f45490bd9dd6
Andrea Scarcella <scarcelan@gmail.com>
2012-04-03 23:06:11 -0700

    Rspec working correctly

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rottenpotatoes::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :movies
-
+  
+  #match ':controller/:id/:action' #=> "patients#show"
+  match '/movies/:id/same_director' => "movies#same_director"
   # Sample resource route with options:
   #   resources :products do
   #     member do
